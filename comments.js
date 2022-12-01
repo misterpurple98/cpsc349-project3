19c62bd8dcc99d1512144b19991843a1d7c43ae2
misterpurple98 <misterpurple98@gmail.com>
2022-12-01 14:52:10 -0800

    Fixed Shuffling, Added methods to handle dealing and clearing game state

--- comments.js
+++ comments.js
@@ -3,12 +3,29 @@ class GameState {
   suits = ["spades", "hearts", "diamonds", "clubs"];
   types = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   
+  player = new Array();
+  dealer = new Array();
+
   constructor() {
     this.makeDeck();
   };
 
+  clearState() {
+    this.player = [];
+    this.dealer = [];
+    this.makeDeck();
+  };
+
+  hitPlayer() {
+    this.player.push(this.gameDeck.pop())
+  };
+
+  hitDealer() {
+    this.dealer.push(this.gameDeck.pop())
+  };
+
   makeDeck() {
-    this.gameDeck.length = 0;
+    this.gameDeck = [];
     for (const suit in this.suits) {
       for (const type in this.types) {
         let card = [this.suits[suit], this.types[type]];
@@ -22,12 +39,9 @@ class GameState {
     for (let i = this.gameDeck.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * i);
       if (j != i) {
-        let tmp = this.gameDeck[i].slice(0);
-        this.gameDeck[i][0] = this.gameDeck[j][0];
-        this.gameDeck[i][1] = this.gameDeck[j][1];
-
-        this.gameDeck[j][0] = tmp[0];
-        this.gameDeck[j][1] = tmp[1];
+       let tmp = this.gameDeck[i];
+       this.gameDeck[i] = this.gameDeck[j];
+       this.gameDeck[j] = tmp;
       }
     }
   };
@@ -44,3 +58,16 @@ game.shuffleDeck();
 console.log(game.deck);
 game.makeDeck();
 console.log(game.deck);
+
+game.shuffleDeck();
+console.log(game.deck);
+game.hitPlayer();
+game.hitDealer();
+console.log(game.player);
+console.log(game.dealer);
+console.log(game.deck);
+
+game.clearState();
+console.log(game.deck);
+console.log(game.player);
+console.log(game.dealer);
