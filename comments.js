ef676152b44e7ec897244be7f6f21b22fcf470e7
misterpurple98 <misterpurple98@gmail.com>
2022-12-01 12:09:10 -0800

    Created GameState to simulate a 52-card Deck
    Shuffle does not work, it seems that no matter what is done in the method it does not actually change the array gameDeck despite reflecting changes in the method itself

--- comments.js
+++ comments.js
@@ -1,128 +1,42 @@
-// https://jsonplaceholder.typicode.com/guide/
-
-async function downloadPosts (page = 1) {
-  const postsURL = `https://jsonplaceholder.typicode.com/posts?_page=${page}`
-  const response = await fetch(postsURL)
-  const articles = await response.json()
-  return articles
-}
-
-async function downloadComments (postId) {
-  const commentsURL = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`
-  const response = await fetch(commentsURL)
-  const comments = await response.json()
-  return comments
-}
-
-async function getUserName (userId) {
-  const userURL = `https://jsonplaceholder.typicode.com/users/${userId}`
-  const response = await fetch(userURL)
-  const user = await response.json()
-  return user.name
-}
-
-function getArticleId (comments) {
-  const article = comments.previousElementSibling
-  const data = article.dataset
-  return data.postId
-}
-
-async function generateArticle (post) {
-  const article = document.createElement('article')
-  const title = document.createElement('h2')
-  const byAuthor = document.createElement('aside')
-  const author = document.createElement('span')
-  const articleP = document.createElement('p')
-
-  article.setAttribute('data-post-id', post.id)
-
-  title.textContent = post.title
-
-  author.textContent = await getUserName(post.userId)
-  byAuthor.textContent = "by "
-  byAuthor.appendChild(author)
-
-  articleP.innerHTML = post.body.split("\n").join("<br>")
-
-  article.appendChild(title)
-  article.appendChild(byAuthor)
-  article.appendChild(articleP)
-  return article
-}
-
-function populateDetails (detail) {
-  const summary = document.createElement('summary')
-  summary.textContent = "See what our readers had to say..."
-  detail.appendChild(summary)
-
-  const sect = document.createElement('section')
-  const header = document.createElement('header')
-  const comHead = document.createElement('h3')
-  comHead.textContent = "Comments"
-  header.appendChild(comHead)
-  sect.appendChild(header)
-  detail.appendChild(sect)
-}
-
-function createComment (comment) {
-  const commentAside = document.createElement('aside')
-
-  const commentText = document.createElement('p')
-  commentText.innerHTML = comment.body.split("\n").join("<br>")
+class GameState {
+  gameDeck = new Array();
+  suits = ["spades", "hearts", "diamonds", "clubs"];
+  types = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   
+  constructor() {
+    this.makeDeck();
+  };
 
-  const commentTitle = document.createElement('p')
-  const commentSm = document.createElement('small')
-  commentSm.innerHTML = comment.name
-
-
-  commentTitle.appendChild(commentSm)
-
-  commentAside.appendChild(commentText)
-  commentAside.appendChild(commentTitle)
-
-  return commentAside
-}
-
-/*
-const details = document.getElementsByTagName('details')
-for (const detail of details) {
-  detail.addEventListener('toggle', async event => {
-    if (detail.open) {
-      const asides = detail.getElementsByTagName('aside')
-      const commentsWereDownloaded = asides.length > 0
-      if (!commentsWereDownloaded) {
-        const articleId = getArticleId(detail)
-        const comments = await downloadComments(articleId)
-        console.log(comments)
+  makeDeck() {
+    this.gameDeck.length = 0;
+    for (const suit in this.suits) {
+      for (const type in this.types) {
+        let card = [this.suits[suit], this.types[type]];
+        this.gameDeck.push(card);
       }
     }
-  })
-}
-*/
+  };
 
-const posts = await downloadPosts(2)
-const main = document.querySelector('main')
-for (const post of posts) {
-  const article = await generateArticle(post)
-  const details = document.createElement('details')
-  populateDetails(details)
-  const commentSection = details.querySelector('section')
-  details.addEventListener('toggle', async event => {
-    if (details.open) {
-      const asides = details.getElementsByTagName('aside')
-      const commentsWereDownloaded = asides.length > 0
-      if (!commentsWereDownloaded) {
-        const articleId = getArticleId(details)
-        const comments = await downloadComments(articleId)
-        console.log(comments)
-        for (const comment of comments) {
-          commentSection.appendChild(createComment(comment))
-        }
+  //Fisher-Yates shuffle implementation
+  shuffleDeck() {
+    for (let i = this.gameDeck.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * i);
+      if (j != i) {
+        let tmp = this.gameDeck[i].slice(0);
+        this.gameDeck.copyWithin(i, j, j+1)
       }
     }
-  })
-  main.appendChild(article)
-  main.appendChild(details)
-}
-console.log(posts)
+  };
+
+  get deck() {
+    return this.gameDeck;
+  };
+};
+
+let game = new GameState();
+
+console.log(game.deck);
+game.shuffleDeck();
+console.log(game.deck);
+game.makeDeck();
+console.log(game.deck);
